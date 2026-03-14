06659f4b5bed17a14051fa5b19ab9bf8adcc7d37
itzreesa <meow@reesa.cc>
2026-03-14 19:37:07 +0100

    components: common: remove unused function

--- components/common.py
+++ components/common.py
@@ -3,23 +3,3 @@ LYRIA_VERSION_MINOR = 3
 LYRIA_VERSION_PATCH = 2
 
 LYRIA_VERSION_FRIENDLY = f"{LYRIA_VERSION_MAJOR}.{LYRIA_VERSION_MINOR}.{LYRIA_VERSION_PATCH}"
-
-def extract_tags(file_data, args) -> tuple:
-  artist = file_data["artist"][0]
-  album = ""
-  if "album" in file_data:
-    album = file_data["album"][0]
-  title = file_data["title"][0]
-
-  if args.artist:
-    artist = args.artist
-  if args.album:
-    album = args.album
-
-  # sanitize!
-  if "/" in artist:
-    artist = artist.replace("/", " ")
-  if "/" in title:
-    title = title.replace("/", " ")
-
-  return artist, album, title
